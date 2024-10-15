8b442bb653cdbaac910d70fde619911cc290f86a
pasindu madubashana <madubashana.pasindu@gmail.com>
2024-10-15 17:05:39 -0400

    Modified page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,74 +19,10 @@ export default function Home() {
                         <li>
                             Code Location: Please complete your answer in{" "}
                             <code>modules/test</code>. You may start your answer
-                            after <code>// Your Test Starts Here</code>
+                            after <code>/Your Test Starts Here</code>
                         </li>
-                        <li>
-                            Component Requirement: This component is essentially
-                            a To-Do List, it should include:
-                            <ul>
-                                <li>
-                                    Input box for users to enter new to do item.
-                                </li>
-                                <li>A button to add the item to the list.</li>
-                                <li>
-                                    Display all the to do items underneath the
-                                    input area.
-                                </li>
-                                <li>
-                                    Every item should have its own "Delete"
-                                    button. Once clicked, only the corresponding
-                                    item will be deleted.
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            State Management: Please use the React Hook{" "}
-                            <code>useState</code> to manage the To-Do List.
-                        </li>
-                        <li>
-                            No Storage Required: The list is not required to be
-                            saved. For example, if the user refresh the page,
-                            the list will be reset.
-                        </li>
-                        <li>
-                            Screen Compatible: Please make sure that it works on
-                            both desktop view (768px plus) and mobile view
-                            (768px less)
-                        </li>
-                        <li>
-                            Styling (bonus): if you would like to add styling,
-                            please add them in{" "}
-                            <code>modules/test/test.module.css</code>
-                        </li>
-                        <li>
-                            Error Handling (bonus): Report the error when the
-                            input is empty.
-                        </li>
-                        <li>Edit (bonus): Allow user to edit the item(s)</li>
+                     
                     </ol>
-                    <p>
-                        If you have any questions, please email{" "}
-                        <a
-                            className="underline"
-                            href={email}
-                            id="questionEmail"
-                            target="_blank">
-                            mzh&#64;
-                            <b className="hidden">
-                                hiddenToPreventFromCrawlers
-                            </b>
-                            letsstopaids
-                            <b className="hidden">
-                                hiddenToPreventFromCrawlers
-                            </b>
-                            &#46;org
-                        </a>
-                    </p>
-                    <p>Note 1: Please add proper comments in your code.</p>
-                    <p>
-                        Note 2:🚫Do NOT use external libraries to complete it.
-                    </p>
                 </div>
             </div>
             <Todo />
